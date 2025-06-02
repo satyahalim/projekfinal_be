ce52907175e4ea6e1128a3c6b81689a30dacac9e
Satyagraha Halim Widiatmojo <160807125+satyahalim@users.noreply.github.com>
2025-06-02 14:52:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const corsOptions = {
   origin: [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://projekfinal-fe-dot-xenon-axe-450704-n3.uc.r.appspot.com/"
+    "https://fe-finalprojek-dot-xenon-axe-450704-n3.uc.r.appspot.com/"
+    
     
     // Tambahkan localhost untuk pengembangan lokal
     // url deploy frontend
